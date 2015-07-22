3264c31ef5c5f1abd5f93951d1b0891b42beb26b
Ville Tainio <tainio.ville@gmail.com>
2015-07-22 22:26:54 +0300

    Display whether NFC is enabled or not

--- app/src/main/java/com/villetainio/travelcardreminder/MainActivity.java
+++ app/src/main/java/com/villetainio/travelcardreminder/MainActivity.java
@@ -1,13 +1,34 @@
 package com.villetainio.travelcardreminder;
 
 import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private TextView infoView;
+    private NfcAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoView = (TextView) findViewById(R.id.viewTest);
+
+        adapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (adapter == null) {
+            Toast.makeText(this, R.string.error_message_no_nfc_on_device, Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        if (!adapter.isEnabled()) {
+            infoView.setText(R.string.error_message_nfc_disabled);
+        } else {
+            infoView.setText(R.string.app_name);
+        }
     }
 }
