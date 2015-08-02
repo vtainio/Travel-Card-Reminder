642b63041f6ba88e712a58e54d32cc1a1010aee2
Ville Tainio <ville.tainio@futurice.com>
2015-08-02 22:08:58 +0300

    Read data from HSL travel card

---
+++ app/src/main/java/com/villetainio/travelcardreminder/ReadCardActivity.java
@@ -0,0 +1,99 @@
+/*
+* Copyright 2015 Ville Tainio
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.villetainio.travelcardreminder;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hsl.cardproducts.TravelCard;
+import com.hsl.example.CardOperations;
+
+import java.io.IOException;
+
+public class ReadCardActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_read_card);
+        storeTravelCardData(getIntent());
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        storeTravelCardData(intent);
+    }
+
+    private TravelCard readCardData(Intent intent) {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        IsoDep isoDep = IsoDep.get(tagFromIntent);
+
+        try {
+            isoDep.connect();
+            return CardOperations.readTravelCardData(isoDep);
+        } catch (IOException ioErr) {
+            Toast err = Toast.makeText(this, ioErr.getMessage(), Toast.LENGTH_LONG);
+            err.show();
+            return new TravelCard(TravelCard.STATUS_NO_HSL_CARD);
+        }
+    }
+
+    private void storeTravelCardData(Intent intent) {
+        TravelCard card = readCardData(intent);
+
+        if (card.getErrorStatus() == TravelCard.STATUS_NO_HSL_CARD) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.error_label_card_read_failure)
+                    .setMessage(R.string.error_message_card_read_failure)
+                    .setNegativeButton(R.string.button_value_close, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            moveToMainActivity();
+                        }
+                    });
+
+            AlertDialog alert = builder.show();
+            alert.show();
+
+        } else {
+            String period1 = CardOperations.getTravelCardPeriod1Validity(card, getApplicationContext());
+            String value = CardOperations.getTravelCardValue(card);
+
+            SharedPreferences cardStorage = getSharedPreferences(getString(R.string.card_storage_name), Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = cardStorage.edit();
+            editor.putString(getString(R.string.card_storage_period), period1);
+            editor.putString(getString(R.string.card_storage_value), value);
+            editor.apply();
+
+            moveToMainActivity();
+        }
+    }
+
+    private void moveToMainActivity() {
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+    }
+}
--- hsl-card-library/src/main/java/com/hsl/example/CardOperations.java
+++ hsl-card-library/src/main/java/com/hsl/example/CardOperations.java
@@ -1,7 +1,7 @@
 /*
  * CardOperations.java
  *
- * Copyright (C) 2012 HSL/HRT (Helsingin seudun liikenne/ Helsinki Region Transport) 
+ * Copyright (C) 2012 HSL/HRT (Helsingin seudun liikenne/ Helsinki Region Transport)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,10 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
+ *
+ * NOTE:
+ * getTravelCardValue modified by Ville Tainio.
  */
 
 package com.hsl.example;
@@ -445,7 +448,8 @@ public class CardOperations
 		String validityArea = mappings.getValidityArea((int) card.getValidityAreaType1(), (int) card.getValidityArea1());
 		
 		//get Calendar instance of Period start date 
-		Calendar periodStartCal1 = Calendar.getInstance(); 
+		Calendar periodStartCal1 = Calendar.getInstance();
+
 		periodStartCal1.setTime (card.getPeriodStartDate1());
 		
 		//get Calendar instance of Period end date 
@@ -526,8 +530,8 @@ public class CardOperations
 	public static String getTravelCardValue(TravelCard card)
 	{
 		//Calculate euros and cents out of value counter
-		String euros = String.valueOf (card.getValueCounter() / 100);
-		String cents = String.valueOf (card.getValueCounter() % 100);
+		int euros = card.getValueCounter() / 100;
+		int cents = card.getValueCounter() % 100;
 		
 		//Build string, cents with 2 digits and leading zeros. Add euro-character at the end. 
 		String value = String.format("%d.%02d \u20ac",euros,cents);
