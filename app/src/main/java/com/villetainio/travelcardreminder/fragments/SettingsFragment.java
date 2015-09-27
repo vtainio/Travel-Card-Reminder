47e9fb07f44bb56f8ac8ca3498e948e73a5773eb
Ville Tainio <ville.tainio@futurice.com>
2015-09-27 19:27:15 +0300

    Add initial fields for settings

--- app/src/main/java/com/villetainio/travelcardreminder/fragments/SettingsFragment.java
+++ app/src/main/java/com/villetainio/travelcardreminder/fragments/SettingsFragment.java
@@ -15,23 +15,96 @@
 */
 package com.villetainio.travelcardreminder.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.villetainio.travelcardreminder.R;
+import com.villetainio.travelcardreminder.TravelCardReminder;
 
 public class SettingsFragment extends Fragment {
+    SharedPreferences cardStorage;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cardStorage = TravelCardReminder.cardStorage;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
-        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+        final View view = inflater.inflate(R.layout.settings_fragment, container, false);
+
+        // Enable notifications is true by default
+        CheckBox enableNotificationsView = (CheckBox) view.findViewById(R.id.enable_notifications);
+        boolean enableNotifications = cardStorage.getBoolean(
+                getString(R.string.settings_enable_notifications),
+                true
+        );
+        enableNotificationsView.setChecked(enableNotifications);
+
+        // Day to start notifications is 3 days before by default.
+        int days = cardStorage.getInt(getString(R.string.settings_days), 3);
+        NumberPicker numberPicker = (NumberPicker) view.findViewById(R.id.days_pick);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(5); //TODO get current days left
+
+        // Time to start notifications is 12.00 by default.
+        int hours = cardStorage.getInt(getString(R.string.settings_hours), 12);
+        int minutes = cardStorage.getInt(getString(R.string.settings_minutes), 0);
+        TimePicker timePicker = (TimePicker) view.findViewById(R.id.time_pick);
+        timePicker.setCurrentHour(hours);
+        timePicker.setCurrentMinute(minutes);
+
+        // Save settings.
+        Button saveButton = (Button) view.findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saveValues(view);
+                Toast.makeText(getActivity().getApplicationContext(),
+                        R.string.success_message_save_settings,
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
         return view;
+    }
+
+    private void saveValues(View view) {
+        SharedPreferences.Editor editor = cardStorage.edit();
+        // Enable notifications
+        editor.putBoolean(
+                getString(R.string.settings_enable_notifications),
+                ((CheckBox) view.findViewById(R.id.enable_notifications)).isChecked()
+        );
+
+        // Days before
+        NumberPicker numberPicker = (NumberPicker) view.findViewById(R.id.days_pick);
+        editor.putInt(
+                getString(R.string.settings_days),
+                numberPicker.getValue()
+        );
+
+        // Time to start
+        TimePicker timePicker = (TimePicker) view.findViewById(R.id.time_pick);
+        editor.putInt(
+                getString(R.string.settings_hours),
+                timePicker.getCurrentHour()
+        );
+        editor.putInt(
+                getString(R.string.settings_minutes),
+                timePicker.getCurrentMinute()
+        );
+
+        editor.apply();
     }
 }
